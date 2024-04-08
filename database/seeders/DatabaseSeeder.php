984b122eac6cdfd1ccedd6851cde9cd832aabae6
canyouhearthemusic <alibektastan04@gmail.com>
2024-04-08 06:17:18 +0500

    feat: implemented roles and permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+            PermissionSeeder::class
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Иван Иванов Иванович',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('pass1234'),
+        ])->assignRole('admin');
+
+        User::factory()->create([
+            'name' => 'Петр Петров Петрович',
+            'email' => 'editor@example.com',
+            'password' => Hash::make('pass1234'),
+        ])->assignRole('editor');
+
+        User::factory()->create([
+            'name' => 'Михаил Михаилов Михаилович',
+            'email' => 'regular@example.com',
+            'password' => Hash::make('pass1234'),
+        ])->assignRole('regular');
     }
 }
---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name' => 'posts.create'])->assignRole('admin');
+        Permission::create(['name' => 'posts.update'])->assignRole('admin', 'editor');
+        Permission::create(['name' => 'posts.delete'])->assignRole('admin');
+    }
+}
